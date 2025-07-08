399de0874ee8ee7035dc48490235e65b9bdeda82
kdmstj <rkddmstj429@gmail.com>
2025-07-08 15:10:30 +0900

    add: UserPoint 도메인의 충전/차감 비즈니스 로직
    - 충전 메서드: 현재 포인트에 요청된 포인트를 더하고, 새로운 UserPoint 인스턴스를 반환하도록 구현
    - 차감 메서드: 현재 포인트에서 요청된 포인트를 빼고, 새로운 UserPoint 인스턴스를 반환하도록 구현
    - 차감 시 요청된 포인트가 현재 포인트보다 클 경우 예외를 발생시키는 로직 추가
    - 포인트 부족 예외 상황을 나타내기 위한 ErrorCode(INSUFFICIENT_POINT) 정의

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,5 +11,12 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+    }
+
+    @ExceptionHandler(value = BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus httpStatus = HttpStatus.valueOf(errorCode.getStatus());
+        return ResponseEntity.status(httpStatus).body(new ErrorResponse(String.valueOf(httpStatus), errorCode.getMessage()));
     }
 }
