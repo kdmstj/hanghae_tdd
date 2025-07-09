b4bc3aee4138c90ea9fdcc13e046570ca1911a47
kdmstj <rkddmstj429@gmail.com>
2025-07-09 11:47:56 +0900

    add: 포인트 컨트롤러 및 유효성 검증 설정
    - 포인트 조회, 포인트 이력 조회, 포인트 충전, 포인트 차감 엔드포인트 추가
    - 포인트 충전/차감 요청 시 0 이상의 양수만 입력 가능하도록 @Valid 적용
    - PointRequest DTO를 생성해 유효성 검사 수행
    - 유효성 검증 실패 시 ErrorResponse 형식으로 반환되도록 AOP 설정
    - 유효성 검사(jakarta.validation) 의존성 추가
    
    (cherry picked from commit 3baa8e836646f05649ddf09254dde22584e21cd4)

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,7 @@
 package io.hhplus.tdd.point;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -7,50 +9,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/point")
+@RequestMapping("/points")
+@RequiredArgsConstructor
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
-    @GetMapping("{id}")
+    @GetMapping("/{userId}")
     public UserPoint point(
-            @PathVariable long id
+            @PathVariable long userId
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.get(userId);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
-    @GetMapping("{id}/histories")
+    @GetMapping("/{userId}/histories")
     public List<PointHistory> history(
-            @PathVariable long id
+            @PathVariable long userId
     ) {
-        return List.of();
+        return pointService.getHistory(userId);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
-    @PatchMapping("{id}/charge")
+    @PatchMapping("{userId}/charge")
     public UserPoint charge(
-            @PathVariable long id,
-            @RequestBody long amount
+            @PathVariable long userId,
+            @RequestBody @Valid PointRequest request
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.charge(userId, request.amount());
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
-    @PatchMapping("{id}/use")
+
+    @PatchMapping("/{userId}/use")
     public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
+            @PathVariable long userId,
+            @RequestBody @Valid PointRequest request
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.use(userId, request.amount());
     }
 }
