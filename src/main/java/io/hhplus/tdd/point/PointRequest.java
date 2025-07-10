73a5cadfe29bd408e2162ff13b62d6db14f4dd25
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2025-07-10 09:58:50 +0900

    Revert "TDD 로 point 와 관련된 요구사항을 해결할 수 있다 (1)"

--- src/main/java/io/hhplus/tdd/point/PointRequest.java
+++
@@ -1,9 +0,0 @@
-package io.hhplus.tdd.point;
-
-import jakarta.validation.constraints.PositiveOrZero;
-
-public record PointRequest(
-        @PositiveOrZero(message = "포인트는 0을 포함한 양수이어야 합니다")
-        long amount
-) {
-}
